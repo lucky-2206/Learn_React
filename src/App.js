517f0b8dd60691adc3c4e1b9e6927f10d220fd7e
lucky_2206 <vaibhavjainlucky98@gmail.com>
2023-03-31 19:24:12 +0530

    Implementation of Hooks using UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { element } from 'prop-types';
 import './App.css';
 import Header from './Components/Header';
-import Movie from './Components/Movie';
-import data from './data.json'
+import React, { useState } from 'react'
+
 function App() {
+  const[num,setnum]=useState(1);
+  function inc(ne)
+  {
+    setnum(num+ne)
+  }
+  function dec(ne)
+  {
+    setnum(num-ne)
+  }
   return (
     <div className="App">
       <Header />
-      <div className='main'>
-        {
-          data.map((element) => {
-            return (
-              <Movie
-                title={element.Title}
-                year={element.Year}
-                img={element.Poster}
-              />
-            )
-          })
-        }
-
+      <div>
+      <p>Demonstration of UseState where in clicking increment button number will be increased by 3 and on decreasing it gets decreased by 2</p>
+        <h1>{num}</h1>
+        <div className='buttons'>
+          <button className='btn' onClick={()=>inc(3)}>Increment</button>
+          <button className='btn' onClick={()=>dec(2)}>Decrement</button>
+        </div>
       </div>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='Header'>
-   <h1>Movies Review</h1>
+   <h1>UseState Intro</h1>
    </div>
   )
 }
